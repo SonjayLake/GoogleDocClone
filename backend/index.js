8e1ab0ce9ca7e3a144353feb06f8cf49d59ae3fb
Sonjay Lake <lakesonjay@gmail.com>
2024-05-20 19:24:14 -0400

    Websocket barebones...

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,38 @@
 const express = require("express");
+const { WebSocketServer } = require("ws");
 
 app = express();
 const port = 4000;
 
+//initialize websocket
+const websocket = new WebSocketServer({ port: 2048 });
 
-app.get("/",(req,res) => {
-    console.log("Backend working");
-    res.send("Backend working");
-})
+//websocket response on connection
+websocket.on("connection", (ws) => {
+  console.log("New client connected!");
 
-app.listen(port, () => {
-    console.log(`Server started, listening on port ${port}`)
-})
+  ws.send("connection established");
+});
+
+//websocket response when message received
+ws.on("message", (data) => {
+  websocket.clients.forEach((client) => {
+    console.log(`distributing message: ${data}`);
+    client.send(`${data}`);
+  });
+});
+
+//websocket response on error
+ws.onerror = function () {
+  console.log("websocket error");
+};
+
+//websocket response on close
+ws.on("close", () => console.log("Client has disconnected!"));
+
+//run server and use websocket
+app
+  .use((req, res) => res.sendFile("/ws-client.html", { root: __dirname }))
+  .listen(port, () => {
+    console.log(`Server started, listening on port ${port}`);
+  });
