a2ee8bbeb12101d8549ab217bbdb04b38faef7ac
Sonjay Lake <lakesonjay@gmail.com>
2024-05-25 01:12:08 -0400

    Text area demo

--- frontend/src/app/components/document/TextArea.js
+++ frontend/src/app/components/document/TextArea.js
@@ -1,40 +1,40 @@
-import React, { useEffect, useRef } from 'react';
-import Quill from 'quill';
-import 'quill/dist/quill.snow.css'; // This imports Quill's default stylesheet
+import React, { useEffect, useRef } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css"; // This imports Quill's default stylesheet
 
 function TextArea() {
-    const quillRef = useRef(null);
-    const quillInstance = useRef(null); //store the instance
+  const quillRef = useRef(null);
+  const quillInstance = useRef(null); //store the instance
 
-    useEffect(() => {
-        if (quillRef.current && !quillInstance.current) {
-            new Quill(quillRef.current, {
-                theme: 'snow', // Specify theme
-                modules: {
-                    toolbar: [
-                        [{ header: '1' }, { header: '2' }, { font: [] }],
-                        [{ size: [] }],
-                        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-                        [{ list: 'ordered'}, { list: 'bullet' }],
-                        ['link', 'image', 'video'],
-                        ['clean']
-                    ],
-                    clipboard: {
-                        matchVisual: false
-                    }
-                },
-                placeholder: 'Compose an epic...',
-                bounds: quillRef.current,
-                scrollingContainer: quillRef.current
-            });
-        }
-    }, []);
+  useEffect(() => {
+    if (quillRef.current && !quillInstance.current) {
+      new Quill(quillRef.current, {
+        theme: "snow", // Specify theme
+        modules: {
+          toolbar: [
+            [{ header: "1" }, { header: "2" }, { font: [] }],
+            [{ size: [] }],
+            ["bold", "italic", "underline", "strike", "blockquote"],
+            [{ list: "ordered" }, { list: "bullet" }],
+            ["link", "image", "video"],
+            ["clean"],
+          ],
+          clipboard: {
+            matchVisual: false,
+          },
+        },
+        placeholder: "Compose an epic...",
+        bounds: quillRef.current,
+        scrollingContainer: quillRef.current,
+      });
+    }
+  }, []);
 
-    return (
-        <div className="text-editor">
-            <div ref={quillRef} />
-        </div>
-    );
+  return (
+    <div className="text-editor">
+      <div ref={quillRef} />
+    </div>
+  );
 }
 
 export default TextArea;
